80d1c7756c9c601cdea3a03494883bd89c039507
sammythedeveloper <justsammy484@gmail.com>
2026-01-25 12:57:36 -0500

    build debug

--- membership-client/src/pages/Admin/AdminUsers.jsx
+++ membership-client/src/pages/Admin/AdminUsers.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "../../utils/axiosInstance";
 import AdminSidebar from "../../components/AdminSidebar";
 
 export default function AdminUsers() {
   const [users, setUsers] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUsers = async () => {
